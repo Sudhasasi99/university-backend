b6a81ee20dd83816d6fcec38b450495e6801d5cc
Sudhasasi99 <100472709+Sudhasasi99@users.noreply.github.com>
2025-07-22 15:25:34 +0530

    Update SecurityConfig.java

--- src/main/java/com/university/university_backend/config/SecurityConfig.java
+++ src/main/java/com/university/university_backend/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        configuration.setAllowedOrigins(Arrays.asList("https://joyful-cajeta-f0bc16.netlify.app/"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true); // optional, if you are using cookies/session
